284ba06b0205e468813a49a609cb3e69dd514d13
MarinaYano <myano2621@gmail.com>
2023-09-14 18:08:13 -0700

    solved ex02.js

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -10,4 +10,19 @@ Write a series of conditional statements that:
 
 */
 
+let musicians = 1;
+
+if(musicians <= 0){
+  console.log("not a group");
+} else if(musicians === 1){
+  console.log("solo");
+} else if (musicians === 2){
+  console.log("duet");
+} else if(musicians === 3){
+  console.log("trio");
+} else if(musicians === 4){
+  console.log("quartet");
+} else {
+  console.log("thi is a large group");
+}
 
