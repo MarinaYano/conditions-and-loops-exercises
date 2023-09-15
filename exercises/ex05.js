c1bbf614274a9d6eab1d1a327f060a466e944ad9
MarinaYano <myano2621@gmail.com>
2023-09-14 19:10:12 -0700

    solved ex05.js

--- exercises/ex05.js
+++ exercises/ex05.js
@@ -26,3 +26,23 @@ var shirtSleeve = 8.47; // size M (medium)
 Then print N/A to the console because the measurements don't all match up with one particular size.
 
 */
+
+let shirtWidth = 27;
+let shirtLength = 33;
+let shirtSleeve = 9.78;
+
+if((shirtWidth >=18  && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve < 8.38)){
+  console.log("size S");
+} else if((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)){
+  console.log("size M");
+} else if ((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88)){
+  console.log("size L");
+} else if((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)){
+  console.log("size XL");
+} else if((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)){
+  console.log("size 2XL");
+} else if (shirtWidth >= 28 && shirtLength >= 34 && shirtSleeve >= 10.13){
+  console.log("size 3XL");
+} else {
+  console.log("N/A");
+}
